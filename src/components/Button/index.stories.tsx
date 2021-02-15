9719d276795284e39e23a1cb2a5cae71cb3b6984
k-kimura <kk.baseball.0824@gmail.com>
2021-02-15 21:46:13 +0900

    Buttonコンポーネントのストーリーファイル作成

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { Button } from "./";
+
+export default {
+  title: "Button",
+  component: Button,
+} as Meta;
+
+type ButtonProps = React.ComponentProps<typeof Button>;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Buttons = Template.bind({});
+
+Buttons.args = {
+  text: "Button",
+  isDisabled: false,
+};
