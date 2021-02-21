1a570fdbe38b18acef68ff519f8b40a4e1e52e1e
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-02-21 19:09:06 +0900

    Iconコンポーネントを簡易的に作成

---
+++ src/components/Icon/index.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { Icon } from './';
+
+export default {
+  title: 'Icon',
+  component: Icon,
+} as Meta;
+
+type CheckboxProps = React.ComponentProps<typeof Icon>;
+
+const Template: Story<CheckboxProps> = (args) => {
+  return <Icon {...args} />;
+};
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  name: 'Icon',
+};
