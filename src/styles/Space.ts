72c0195ff9d8992c3d43373d9f9445341aa34039
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-03-26 01:52:59 +0900

    utils関数にコードコメントを追加

--- src/styles/Space.ts
+++ src/styles/Space.ts
@@ -1,11 +1,17 @@
-import { css } from "styled-components";
+import { css } from 'styled-components';
 
 const minSpaceSize = 4 as const;
 
+/**
+ * 4の倍数を用いてpx指定を行うときに用いる関数
+ */
 export const getSpace = (val: number): string => {
   return `${minSpaceSize * val}px`;
 };
 
+/**
+ * 正方形を作成したいときに用いる関数
+ */
 export const getSquareCss = (size: number) => {
   const value = `${size}px`;
 
