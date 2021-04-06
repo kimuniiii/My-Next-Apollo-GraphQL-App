52dffa8667ff6ba87d467a40babf13fd9b1d376d
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-04-07 00:28:43 +0900

    Next.jsで使用するLayoutコンポーネントを作成

---
+++ src/layouts/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export const CommonLayout = () => (
+  <Html>
+    <Head />
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
