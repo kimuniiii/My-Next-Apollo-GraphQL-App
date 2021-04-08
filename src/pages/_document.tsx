23f7e2497fd361c25dcf80dde7746537d3182775
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-04-09 02:40:31 +0900

    styled-componentsのGitHubを見てDocumentコンポーネントをリファクタ

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import Document, { DocumentContext } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+import Document from 'next/document';
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-import { DefaultLayout } from 'src/layouts/index';
-class CommonDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-  render() {
-    return <DefaultLayout />;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default CommonDocument;
