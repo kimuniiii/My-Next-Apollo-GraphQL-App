3a3d425c15d05ae30d9d1195001c14ab973b92ee
Kimura Kazuki <kk.baseball.0824@gmail.com>
2021-04-10 15:32:53 +0900

    TopページのUIの背景色を変更

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -6,19 +6,57 @@ import styled from 'styled-components';
 
 const TopPage: NextPage = () => {
   return (
-    <>
+    <StRoot>
       <Head>
         <title>Topページ</title>
       </Head>
-      <StTitle>Topページ</StTitle>
-      <Link href="/posts">
-        <a>First Postページへ遷移します</a>
-      </Link>
-    </>
+      <StFirstSection>
+        <StTitle>Topページ</StTitle>
+        <Link href="/posts">
+          <a>First Postページへ遷移します</a>
+        </Link>
+      </StFirstSection>
+      <StSecondSection>
+        <h1>Slide2</h1>
+      </StSecondSection>
+      <StThirdSection>
+        <h1>Slide3</h1>
+      </StThirdSection>
+    </StRoot>
   );
 };
 
 export default TopPage;
+
+const StRoot = styled.div``;
+
+const StFirstSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #1fa;
+`;
+
+const StSecondSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f2ff3b;
+  opacity: 0.8;
+`;
+
+const StThirdSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #6bdcff;
+`;
 
 const StTitle = styled.h1`
   color: ${({ theme }) => theme.colors.primary};
