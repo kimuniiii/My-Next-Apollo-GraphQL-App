4761fdba7d74fb9e8a20977d364ccadefb9061f9
k-kimura <kk.baseball.0824@gmail.com>
2021-11-01 17:38:20 +0900

    pages で 100枚の画像を描画

--- src/pages/lazy-image/index.tsx
+++ src/pages/lazy-image/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import React from 'react';
 
 import { LazyImage } from 'src/components/atoms/LazyImage';
 import { Template } from 'src/components/templates/Template';
@@ -11,12 +12,16 @@ const ImagePracticeComponent: NextPage = () => {
         <title>画像最適化検証ページ</title>
       </Head>
       <Template>
-        <LazyImage src='/images/sample01.jpeg' alt='sample01' width={100} height={100} />
-        <LazyImage src='/images/sample02.jpeg' alt='sample02' width={100} height={100} />
-        <LazyImage src='/images/sample03.jpeg' alt='sample03' width={100} height={100} />
+        {[...Array(100)].map((_, idx) => {
+          return testData.map((cur) => {
+            return <LazyImage src={cur.src} alt='sample01' width={100} height={100} />;
+          });
+        })}
       </Template>
     </>
   );
 };
 
 export default ImagePracticeComponent;
+
+const testData = [{ src: '/images/sample01.jpeg' }];
