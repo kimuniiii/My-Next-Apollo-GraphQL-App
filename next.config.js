8310cd518a7ae4a92fce5742f7cfdf02163b3f84
k-kimura <kk.baseball.0824@gmail.com>
2021-11-03 19:18:16 +0900

    add next-compose-plugins in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const bundleAnalyzer = require('@next/bundle-analyzer');
+const withPlugins = require('next-compose-plugins');
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withPlugins([withBundleAnalyzer]);
