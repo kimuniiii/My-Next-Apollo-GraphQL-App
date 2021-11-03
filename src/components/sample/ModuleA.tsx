5a57eb32a7dc0d38920f82f7b49ed8e3bfd5bba5
k-kimura <kk.baseball.0824@gmail.com>
2021-11-03 19:17:56 +0900

    create ModuleA and ModuleB and ModuleC

---
+++ src/components/sample/ModuleA.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+
+/**
+ * @概要 どこにもインポートされていないため、デッドコード
+ */
+export const ModuleA: FC = () => {
+  return <p>ModuleA</p>;
+};
---
+++ src/pages/sample/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const SamplePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Next.js練習用サンプルページ</title>
+      </Head>
+      <header>Header</header>
+      <main>SamplePage</main>
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default SamplePage;
