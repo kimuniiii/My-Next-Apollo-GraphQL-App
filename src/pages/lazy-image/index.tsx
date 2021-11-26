55e87001590d039f54e8e5dc6e753ed99c133b37
k-kimura <kk.baseball.0824@gmail.com>
2021-11-26 10:35:32 +0900

    Imageコンポーネントを使うと画像サイズは小さくなる

--- src/pages/lazy-image/index.tsx
+++ src/pages/lazy-image/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
 import Head from 'next/head';
 import React from 'react';
 import styled from 'styled-components';
@@ -13,26 +14,83 @@ const ImagePracticeComponent: NextPage = () => {
         <title>画像最適化検証ページ</title>
       </Head>
       <Template>
-        <StImageContainer>
-          {[...Array(500)].map((_, idx) => {
-            return (
-              <LazyImage
-                key={idx}
-                src={`https://picsum.photos/id/${idx}/160/160`}
-                alt={`Alt Text ${idx}`}
-                width='400px'
-                height='400px'
-                options={{ rootMargin: '300px' }}
-              />
-            );
-          })}
-        </StImageContainer>
+        <StFlexColumn>
+          <h1>通常のimgタグ</h1>
+          <StImageContainer>
+            <img
+              src='https://picsum.photos/id/200/200/200'
+              alt='Alt Text 200'
+              width='200px'
+              height='200px'
+            />
+            <img
+              src='https://picsum.photos/id/201/200/200'
+              alt='Alt Text 201'
+              width='200px'
+              height='200px'
+            />
+            <img
+              src='https://picsum.photos/id/202/200/200'
+              alt='Alt Text 202'
+              width='200px'
+              height='200px'
+            />
+          </StImageContainer>
+        </StFlexColumn>
+
+        <StFlexColumn>
+          <h1>Next.js Imageコンポーネント</h1>
+          <StImageContainer>
+            <Image
+              src='https://picsum.photos/id/203/200/200'
+              alt='Alt Text 203'
+              width='200px'
+              height='200px'
+            />
+            <Image
+              src='https://picsum.photos/id/204/200/200'
+              alt='Alt Text 204'
+              width='200px'
+              height='200px'
+            />
+            <Image
+              src='https://picsum.photos/id/206/200/200'
+              alt='Alt Text 206'
+              width='200px'
+              height='200px'
+            />
+          </StImageContainer>
+        </StFlexColumn>
+
+        <StFlexColumn>
+          <h1>LazyImageコンポーネント</h1>
+          <StImageContainer>
+            {[...Array(500)].map((_, idx) => {
+              return (
+                <LazyImage
+                  key={idx}
+                  src={`https://picsum.photos/id/${idx}/200/200`}
+                  alt={`Alt Text ${idx}`}
+                  width='200px'
+                  height='200px'
+                  options={{ rootMargin: '300px' }}
+                />
+              );
+            })}
+          </StImageContainer>
+        </StFlexColumn>
       </Template>
     </>
   );
 };
 
 export default ImagePracticeComponent;
+
+const StFlexColumn = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const StImageContainer = styled.section`
   display: flex;
