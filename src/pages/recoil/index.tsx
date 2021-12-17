4ccec5f8185613adeb7c3b30d3b1e21da9516af2
k-kimura <kk.baseball.0824@gmail.com>
2021-12-17 10:41:32 +0900

    1画面1ストア運用のイメージを理解

--- src/pages/recoil/index.tsx
+++ src/pages/recoil/index.tsx
@@ -1,7 +1,8 @@
-import { RecoilRoot, atom, selector } from 'recoil';
+import { RecoilRoot, atom } from 'recoil';
 import { SampleRecoilTextInput } from 'src/components/recoil/SampleTextInput';
 
 const SampleRecoil = () => {
+  // これをstore.tsに格納する運用方法が良いか？
   const textState = atom({
     key: 'textState',
     default: '',
