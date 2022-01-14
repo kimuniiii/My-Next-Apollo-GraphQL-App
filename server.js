b0513927b430e448a0dd9acee34fc71af8cd778b
k-kimura <kk.baseball.0824@gmail.com>
2022-01-14 13:12:55 +0900

    express.static の 使用方法 完全理解

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 // 静的アセットファイルを格納しているディレクトリを一気に読み込む
+// http://127.0.0.1:8080/lazy-image.html でアクセス可能になる
+// .html拡張子 を付け忘れるとエラーになるので注意する
 app.use(express.static('out'));
 
 // 以下のGETリクエスト処理を記載することで動的ルーティングが可能になる
