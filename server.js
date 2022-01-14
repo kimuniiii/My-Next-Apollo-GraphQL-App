6985448f934675c471a6b9f8b32c5deb6f0c96ef
k-kimura <kk.baseball.0824@gmail.com>
2022-01-14 12:08:21 +0900

    Express で ルーティング処理 を行う

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Express');
+  res.sendFile(__dirname + '/out/index.html');
+});
+
+app.get('/main/*', (req, res) => {
+  res.sendFile(__dirname + '/out/main/[test].html');
 });
 
 app.listen(port, hostname, () => {
