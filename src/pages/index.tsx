80e06199fabaac2cd839b77bb2b06442ad84974a
k-kimura <kk.baseball.0824@gmail.com>
2022-01-29 12:49:15 +0900

    TopPageに各ページへ遷移できるリンクを配置する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,42 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { ColorPalette } from 'src/styles/Color';
+import styled from 'styled-components';
 
 const TopPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Next.js練習用トップページ</title>
+        <title>技術検証用トップページ</title>
       </Head>
-      <header>Header</header>
-      <main>TopPage</main>
-      <footer>Footer</footer>
+
+      <StDivTopFlexContainer>
+        <Link href='/lazy-image' passHref>
+          <StAnchorLink>LazyImageの検証ページへ遷移する</StAnchorLink>
+        </Link>
+        <Link href='/memo' passHref>
+          <StAnchorLink>Reactのメモ化の検証ページへ遷移する</StAnchorLink>
+        </Link>
+        <Link href='/recoil' passHref>
+          <StAnchorLink>Recoilの検証ページへ遷移する</StAnchorLink>
+        </Link>
+        <Link href='/virtual-scroll' passHref>
+          <StAnchorLink>仮想スクロールの検証ページへ遷移する</StAnchorLink>
+        </Link>
+      </StDivTopFlexContainer>
     </>
   );
 };
 
 export default TopPage;
+
+const StDivTopFlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const StAnchorLink = styled.a`
+  color: ${ColorPalette.Blue100};
+`;
