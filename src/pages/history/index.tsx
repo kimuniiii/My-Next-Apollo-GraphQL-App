aa77db04901995ea02b145f91fe94f18e6ae6c38
k-kimura <kk.baseball.0824@gmail.com>
2022-04-21 12:49:23 +0900

    history.pushState の挙動確認

---
+++ src/pages/history/index.tsx
@@ -0,0 +1,61 @@
+import { useRouter } from 'next/router';
+
+const History = () => {
+  const router = useRouter();
+
+  const historyBack = () => {
+    window.history.back();
+  };
+
+  const goToMemoPage = () => {
+    router.push('/memo');
+  };
+
+  const historyPushState = () => {
+    const state = { page_id: 1, user_id: 1 };
+    const url = new URL(window.location.href);
+    url.searchParams.set('foo', 'bar');
+    window.history.pushState(state, '', url);
+  };
+
+  const historyPushState2 = () => {
+    const state = { page_id: 2, user_id: 2 };
+    const url = new URL(window.location.href);
+    url.searchParams.set('baseball', 'soccer');
+    window.history.pushState(state, '', url);
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+      <section style={{ border: '1px solid red' }}>
+        <h1>History Back</h1>
+        <button type='button' onClick={historyBack}>
+          戻るボタン
+        </button>
+      </section>
+
+      <section style={{ border: '1px solid red' }}>
+        <h1>Memo画面へ遷移</h1>
+        <button type='button' onClick={goToMemoPage}>
+          Memo画面へ遷移ボタン
+        </button>
+      </section>
+
+      <section style={{ border: '1px solid red' }}>
+        <h1>History pushState ?foo=bar を付与する</h1>
+        <button type='button' onClick={historyPushState}>
+          pushStateボタン
+        </button>
+      </section>
+
+      <section style={{ border: '1px solid red' }}>
+        <h1>History pushState ?baseball=soccer を付与する</h1>
+        <button type='button' onClick={historyPushState2}>
+          pushStateボタン
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default History;
